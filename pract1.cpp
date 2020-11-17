51cb787e7782acd234b362c28a748e4a02856d77
Nilesh95611 <74341526+Nilesh95611@users.noreply.github.com>
2020-11-17 14:28:31 +0530

    Add files via upload

---
+++ pract1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+   int num1,num2;
+   cout <<"enter first num:";
+   cin>>num1;
+   cout<<"enter second num:";
+   cin>>num2;
+   cout<<"sum of num:"<<(num1+num2);
+   return 0;
+}
