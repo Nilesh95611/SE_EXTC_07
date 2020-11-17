759b6884554859ff08b580e734cd0cc5d12010f0
Nilesh95611 <74341526+Nilesh95611@users.noreply.github.com>
2020-11-17 15:01:21 +0530

    Add files via upload

---
+++ pract6.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+class Student
+{
+  public:
+    string name;
+};
+int main(){
+  Student obj ;
+  obj.name="Nilesh Chauhan";
+  cout<<obj.name;
+  
+}
+
+  
