8936a54d13915c7e77d0ea6b030830536d60901d
Nilesh95611 <74341526+Nilesh95611@users.noreply.github.com>
2020-11-17 14:45:48 +0530

    Add files via upload

---
+++ pract2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+   int a;
+   cout<<"Enter the number:";
+   cin>>a;
+   cout<<"You enterd: "<<a;
+   return 0;
+}
